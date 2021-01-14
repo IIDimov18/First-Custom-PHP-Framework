3ebf8596bb4ef59ff62cfe60f770a0159fa9b91a
Ivan Dimov <iidimov18@codingburgas.bg>
2021-01-14 12:31:49 +0200

    Create Application and Router 🐘

---
+++ core/Application.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application
+{
+    public Router $router;
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once
+
+$app = new Application();
+
+
+$app->$router->get('/',function(){
+   return 'Hello World!';
+});
+
+$app->run();
