8124906bbef058de97d81df878a44f2d4338c30a
Ivan Dimov <iidimov18@codingburgas.bg>
2021-01-14 13:44:26 +0200

    Enable auto loading using composer 🐘

--- core/Application.php
+++ core/Application.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace app\core;
+
 class Application
 {
     public Router $router;
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
 
 $app = new Application();
 
