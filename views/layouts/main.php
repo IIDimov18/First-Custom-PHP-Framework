ac9ffec71a7f0d5d5c0923f2e0a34ee969afcc29
Ivan Dimov <iidimov18@codingburgas.bg>
2021-01-20 14:31:55 +0200

    Start Registration 🐘

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,7 +29,17 @@
                 <a class="nav-link" href="/contact">Contact us</a>
             </li>
         </ul>
+        <ul class="navbar-nav ml-autbo">
+            <li class="nav-item active">
+                <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/register">Register</a>
+            </li>
+        </ul>
     </div>
+
+
 </nav>
 
 <div class="container">
