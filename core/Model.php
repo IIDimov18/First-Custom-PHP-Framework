3ea4e7528a2a3dae0ecde7c50b0d98f42304fcbf
Ivan Dimov <iidimov18@codingburgas.bg>
2021-01-22 10:12:51 +0200

    Implement Models 🐘

---
+++ core/Model.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace app\core;
+
+
+abstract class Model
+{
+//    public const RULE_REQUIRED='required';
+//    public const RULE_EMAIL='email';
+//    public const RULE_MIN='min';
+//    public const RULE_MAX='max';
+//    public const RULE_REQUIRED='required';
+    public function loadData($data)
+    {
+        foreach($data as $key => $value){
+            if (property_exists($this, $key)){
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    abstract public function rules(): array;
+
+    }
+
+    public function validate($data)
+    {
+
+    }
+}
